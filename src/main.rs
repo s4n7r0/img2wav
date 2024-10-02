e2538d7e5b446360927cb8c8aab0133906cf2fc9
s4n7r2 <s4n7r0@gmail.com>
2024-10-02 20:54:41 +0200

    fixed bug, now defaults to converting to stereo

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env::args, fs::File, i16, io::{Read, Write}, usize, vec};
+use std::{env::args, fs::File, i16, i32, io::{Read, Write}, usize, vec};
 
 use hound::{self, WavReader};
 use image::{self, ImageReader};
@@ -12,7 +12,7 @@ fn show_help(path: &String) {
     println!("");
     println!("Supported formats: 16-bit .wav, 24-bit .wav, .png, .jpg, .jpeg, .gif, .bmp, .webp");
     println!("");
-    println!("Converting a 16bit wav produces an image in RGB565 mode");
+    println!("Converting a 16bit wav produces an image ign RGB565 mode");
     println!("");
     println!("Options:");
     println!("      -i, --input  [PATH]                   Input file");
@@ -23,16 +23,25 @@ fn show_help(path: &String) {
     println!("      -g, --grayscale                       Outputs image in grayscale");
     println!("      -r, --rotate [VALUE]                  Rotates RGB");
     println!("If converting from image to wav:");
-    println!("      -s, --stereo                          Outputs audio with 2 channels");
+    println!("      -m, --mono                            Output audio with 1 channel");
     println!("      -g, --grayscale                       Interpretes image as if it was in Grayscale");
+    println!("      -sr, --sample-rate                    Outputs audio in specified sample rate");
     println!("      -16                                   Outputs audio in 16bit, assuming that image is in RGB565 mode");
     println!("");
 }
 
 fn process_path(args_struct: &mut Args, path: &String) -> bool {
 
-    if File::open(path).is_err() {println!("Failed to open file!"); return false};
+    if File::open(path).is_err() {
+        println!("Failed to open file!"); 
+        let file = File::options().write(true).open(path);
+        if file.is_err() {
+            if File::create_new(path).is_err() {println!("file can't be created or written to"); return false};
+            println!("file created");
+        };
+    };
     
+
     if args_struct.input.is_empty() {
         match path.split('.').last().unwrap() {
             //TODO: find a way to support all images available in the image crate
@@ -42,17 +51,12 @@ fn process_path(args_struct: &mut Args, path: &String) -> bool {
         }
     }
 
-    let file = File::options().write(true).open(path);
-    if file.is_err() {
-        if File::create_new(path).is_err() {println!("file can't be created or written to"); return false};
-        println!("file created");
-    };
     
-    println!("input: {}", path);
+    println!("path: {}", path);
     true
 }
 
-fn process_args(args_struct: &mut Args) {
+fn process_args(args_struct: &mut Args) -> bool {
     let args: Vec<String> = args().collect();
 
     let mut arg = args.iter().peekable();
@@ -61,7 +65,7 @@ fn process_args(args_struct: &mut Args) {
 
     arg.next();
 
-    if arg.len() == 0 {show_help(&file_path); return;};
+    if arg.len() == 0 {show_help(&file_path); return false;};
     
     loop {
         let current_arg = match arg.peek() {
@@ -77,14 +81,14 @@ fn process_args(args_struct: &mut Args) {
                 args_struct.is_img = true;
                 args_struct.input = current_arg.to_string();
                 args_struct.output = current_arg.split_at(current_arg.rfind('.').unwrap()).0.to_string() + ".wav";
-                break;
+                return true;
             },
             "wav" => {
                 println!("file is a wav"); 
                 args_struct.is_wav = true;
                 args_struct.input = current_arg.to_string();
                 args_struct.output = current_arg.split_at(current_arg.rfind('.').unwrap()).0.to_string() + ".png";
-                break;
+                return true;
             },
             _ => ()
         }
@@ -95,9 +99,10 @@ fn process_args(args_struct: &mut Args) {
 
                 let path = match arg.next() {
                     Some(x) => x,
-                    None => {println!("-i requires an input file!"); return;}
+                    None => {println!("-i requires an input file!"); return false;}
                 };
-                if !process_path(args_struct, path) {return;};
+
+                if !process_path(args_struct, path) {return false;};
                 args_struct.input = path.to_string();
             },
             "-o" | "--output" => {
@@ -105,10 +110,10 @@ fn process_args(args_struct: &mut Args) {
                 
                 let path = match arg.next() {
                     Some(x) => x,
-                    None => {println!("-o requires an output path!"); return;}
+                    None => {println!("-o requires an output path!"); return false;}
                 };
                 
-                if !process_path(args_struct, path) {return;};
+                if !process_path(args_struct, path) {return false;};
                 args_struct.output = path.to_string();
             },
             "-d" | "--dimensions"  => {
@@ -116,24 +121,24 @@ fn process_args(args_struct: &mut Args) {
 
                 let res = match arg.next() {
                     Some(x) => x,
-                    None => {println!("-d requires a resolution in <width>x<height> format!"); return;}
+                    None => {println!("-d requires a resolution in <width>x<height> format!"); return false;}
                 };
 
                 let index = res.find('x').unwrap();
                 let (x, mut y) = res.split_at(index);
                 y = y.trim_start_matches('x');
                 
-                args_struct.dimensions[0] = x.trim().parse().unwrap();
-                args_struct.dimensions[1] = y.trim().parse().unwrap();
+                args_struct.dimensions[0] = x.trim().parse().expect("x is not a number!");
+                args_struct.dimensions[1] = y.trim().parse().expect("y is not a number!");
 
-                if args_struct.dimensions[0] <= 0 || args_struct.dimensions[1] <= 0 {println!("Invalid dimensions!"); return;}
+                if args_struct.dimensions[0] <= 0 || args_struct.dimensions[1] <= 0 {println!("Invalid dimensions!"); return false;}
 
                 println!("{}x{}", x, y);
 
             },
-            "-s" | "--stereo" => {
-                println!("using stereo");
-                args_struct.stereo = true;
+            "-m" | "--mono" => {
+                println!("using mono");
+                args_struct.mono = true;
             },
             "-g" | "--grayscale" => {
                 println!("using grayscale");
@@ -143,12 +148,24 @@ fn process_args(args_struct: &mut Args) {
                 println!("using rotate");
                 
                 let value: usize = match arg.next() {
-                    Some(x) => x.trim().parse().expect("Invalid digit!"),
-                    None => {println!("-r requires a number!"); return;}
+                    Some(x) => x.trim().parse().expect("Invalid number!"),
+                    None => {println!("-r requires a number!"); return false;}
                 };
                 let value = value.clamp(0, 3);
 
                 args_struct.rotate = value;
+            }
+            "-sr" | "--sample-rate" => {
+                println!("using sample rate");
+                
+                let value: u32 = match arg.next() {
+                    Some(x) => x.trim().parse().expect("Invalid number!"),
+                    None => {println!("-s requires a number!"); return false;}
+                };
+
+                if value <= 0 || value > 192000 {println!("Sample rate not supported!"); return false;}
+
+                args_struct.samplerate = value;
             }
             "-16" => {
                 println!("output as a 16 bit wav");
@@ -160,13 +177,14 @@ fn process_args(args_struct: &mut Args) {
                 if !args_struct.input.is_empty() {continue};
 
                 show_help(&file_path);
-                return;
+                return false;
             }
             _ => {
                 println!("Unrecognized argument: {}", current_arg);
             }
         }
     }
+    true
 }
 
 fn wav_to_img(path: &String, args: &Args) {
@@ -179,14 +197,12 @@ fn wav_to_img(path: &String, args: &Args) {
     wav_as_file.read_to_end(&mut wav_bytes).unwrap();
     println!("wav bytes: {}", wav_bytes.len());
     
-    let mut wav = WavReader::open(&path).expect("Failed to open file");
-    let mut wav_length: f32 = (wav.duration() - 0x50) as f32;
-    // subtracting 0x50 cause wav.duration() returns the size of the WHOLE file and not the data chunk, 
-    // which leads to invalid resolutions of images
+    let mut wav = WavReader::open(&path).unwrap();
+    let mut wav_length: f32 = wav.duration() as f32;
 
-    println!("wav length: {}", wav_bytes.len());
+    println!("wav length: {}", wav_length);
     
-    if args.grayscale { wav_length *= 3 as f32; }
+    if args.grayscale { wav_length *= (3 - (wav.spec().bits_per_sample == 16) as u32) as f32; }
     wav_length *= wav.spec().channels as f32;
     
     let img_dimensions = wav_length.sqrt().ceil() as u32;
@@ -210,11 +226,8 @@ fn wav_to_img(path: &String, args: &Args) {
     let img_size = img_dimensions.0 * img_dimensions.1; 
     if img_size < wav.duration() {println!("Dimensions too small! Pic: {} < Samples: {}", img_size, wav.duration()); panic!();};
 
-    // if args.grayscale {img_dimensions.0 *= 3; img_dimensions.1 *= 3;};
     let mut img = image::RgbImage::new(img_dimensions.0, img_dimensions.1);
-    
-    // let mut img_gray = image::GrayImage::new(img_dimensions.0*3, img_dimensions.1*3);
-    
+        
     println!("pic res: {} x {}", img_dimensions.0, img_dimensions.1);
 
     let mut sample_bytes: [u8; 4];
@@ -246,7 +259,7 @@ fn wav_to_img(path: &String, args: &Args) {
                     assert_eq!(sample_bytes[3], 0);
                 }
         }
-        _ => { println!("Unsupported bits per sample!"); return;}
+        _ => { println!("Unsupported bits per sample!"); panic!();}
     }
 
     let mut sample_iterator = sample_array.iter().peekable();
@@ -269,7 +282,6 @@ fn wav_to_img(path: &String, args: &Args) {
             if wav.spec().bits_per_sample == 16 && index_gray >= 2 {index_gray = 0; sample_iterator.next();};
 
             if index_gray >= 3 {index_gray = 0; sample_iterator.next();};
-            // println!("{}", rgb_array[0]);
         }
         img.save(&args.output).expect("Cant save file!")
     } else {
@@ -288,7 +300,7 @@ fn wav_to_img(path: &String, args: &Args) {
                 //         rrrrrggg   gggbbbbb      
                 value.swap(0, 1); //incorrect otherwise idk why but whatever
                 let mut value_r = value[0] >> 3;
-                let mut value_g: u8 = (value[0].rotate_right(3) >> 5) * 2_u8.pow(3) + value[1] >> 5;
+                let mut value_g = ((value[0].rotate_right(3) >> 5) << 3) + (value[1] >> 5);
                 let mut value_b = value[1].rotate_right(5) >> 3;
                 value_r <<= 3; value_g <<= 2; value_b <<= 3; // to get colors from 0 - 255
 
@@ -316,8 +328,8 @@ fn img_to_wav(img: image::ImageBuffer<image::Rgb<u8>, Vec<u8>>, path: &String, a
     println!("{}", img_size);
 
     let wav = hound::WavSpec {
-        channels: 1 + args.stereo as u16,
-        sample_rate: 44100,
+        channels: 2 - args.mono as u16,
+        sample_rate: args.samplerate,
         bits_per_sample: 24 - (8 * args.bit16 as u16),
         sample_format: hound::SampleFormat::Int
     };
@@ -357,17 +369,27 @@ fn img_to_wav(img: image::ImageBuffer<image::Rgb<u8>, Vec<u8>>, path: &String, a
     }
 
     if args.bit16 {
-        for pixel in &pixel_array {
-            let (mut r, mut g, mut b) = (pixel[0], pixel[1], pixel[2]);
-            r >>= 3; g >>= 2; b >>= 3;
-            let mut sample_to_write: u16 = r as u16;
-            sample_to_write <<= 6;
-            sample_to_write += g as u16;
-            sample_to_write <<= 5;
-            sample_to_write += b as u16;
+        if args.grayscale {
+            for pixel in &pixel_array {
+                let sample_to_write: u16 = ((pixel[1] as u16) << 8) + pixel[0] as u16;
             
-            let sample_to_write: i32 = sample_to_write as i32 + i16::MIN as i32; //turn it into a value between 24bit integer MIN and MAX 
-            wav.write_sample(sample_to_write).expect("Cant write sample");
+                let sample_to_write: i16 = (sample_to_write as i32 + i16::MIN as i32) as i16; //turn it into a value between 16bit integer MIN and MAX 
+                wav.write_sample(sample_to_write).expect("Cant write sample");
+            }
+        } else {
+            for pixel in &pixel_array {
+                let (mut r, mut g, mut b) = (pixel[0], pixel[1], pixel[2]);
+                r >>= 3; g >>= 2; b >>= 3;
+                let mut sample_to_write: u16 = r as u16;
+                sample_to_write <<= 6;
+                sample_to_write += g as u16;
+                sample_to_write <<= 5;
+                sample_to_write += b as u16;
+                
+                let sample_to_write: i32 = sample_to_write as i32 + i16::MIN as i32; //turn it into a value between 16bit integer MIN and MAX 
+                wav.write_sample(sample_to_write).expect("Cant write sample");
+            }
+
         }
     } else {
         for pixel in &pixel_array {
@@ -387,42 +409,32 @@ fn img_to_wav(img: image::ImageBuffer<image::Rgb<u8>, Vec<u8>>, path: &String, a
     //writes a custom header where it specifes picture resolution 
     //looks bad maybe make it better in the future?
     let mut cursor = 0x04;
-    let mut riff_size: [u8; 4] = [0, 0, 0, 0];
-    riff_size[0] = *wav_buf.get(cursor).unwrap(); cursor += 1; 
-    riff_size[1] = *wav_buf.get(cursor).unwrap(); cursor += 1; 
-    riff_size[2] = *wav_buf.get(cursor).unwrap(); cursor += 1; 
-    riff_size[3] = *wav_buf.get(cursor).unwrap();
-    let mut riff_size: u32 = u32::from_ne_bytes(riff_size);
-    riff_size += 16;
 
-    let riff_size: [u8; 4] = riff_size.to_ne_bytes();
+    let mut new_riff_size: [u8; 4] = [0, 0, 0, 0];
+    new_riff_size[0] = *wav_buf.get(cursor).unwrap(); cursor += 1; 
+    new_riff_size[1] = *wav_buf.get(cursor).unwrap(); cursor += 1; 
+    new_riff_size[2] = *wav_buf.get(cursor).unwrap(); cursor += 1; 
+    new_riff_size[3] = *wav_buf.get(cursor).unwrap();
+    let new_riff_size: [u8; 4] = (u32::from_ne_bytes(new_riff_size) + 16).to_ne_bytes();
+
     let mut old_riff_size: [u8; 4] = [0, 0, 0, 0];
 
     cursor = 0x04;
-    old_riff_size[0] = std::mem::replace(&mut wav_buf[cursor], riff_size[0]); cursor += 1;
-    old_riff_size[1] = std::mem::replace(&mut wav_buf[cursor], riff_size[1]); cursor += 1;
-    old_riff_size[2] = std::mem::replace(&mut wav_buf[cursor], riff_size[2]); cursor += 1;
-    old_riff_size[3] = std::mem::replace(&mut wav_buf[cursor], riff_size[3]);
+    old_riff_size[0] = std::mem::replace(&mut wav_buf[cursor], new_riff_size[0]); cursor += 1;
+    old_riff_size[1] = std::mem::replace(&mut wav_buf[cursor], new_riff_size[1]); cursor += 1;
+    old_riff_size[2] = std::mem::replace(&mut wav_buf[cursor], new_riff_size[2]); cursor += 1;
+    old_riff_size[3] = std::mem::replace(&mut wav_buf[cursor], new_riff_size[3]);
 
-    println!("old riff size: {}, new riff size {}", u32::from_ne_bytes(old_riff_size), u32::from_ne_bytes(riff_size));
+    println!("old riff size: {}, new riff size {}", u32::from_ne_bytes(old_riff_size), u32::from_ne_bytes(new_riff_size));
 
-    let mut cursor = 0x3C;
-    wav_buf.insert(cursor, b'i'); cursor += 1;
-    wav_buf.insert(cursor, b'2'); cursor += 1;
-    wav_buf.insert(cursor, b'w'); cursor += 1;
-    wav_buf.insert(cursor, b' '); cursor += 1;
-    wav_buf.insert(cursor, 8); cursor += 1;
-    wav_buf.insert(cursor, 0); cursor += 1;
-    wav_buf.insert(cursor, 0); cursor += 1;
-    wav_buf.insert(cursor, 0); cursor += 1;
-    wav_buf.insert(cursor, img_x.to_ne_bytes()[0]); cursor += 1;
-    wav_buf.insert(cursor, img_x.to_ne_bytes()[1]); cursor += 1;
-    wav_buf.insert(cursor, img_x.to_ne_bytes()[2]); cursor += 1;
-    wav_buf.insert(cursor, img_x.to_ne_bytes()[3]); cursor += 1;
-    wav_buf.insert(cursor, img_y.to_ne_bytes()[0]); cursor += 1;
-    wav_buf.insert(cursor, img_y.to_ne_bytes()[1]); cursor += 1;
-    wav_buf.insert(cursor, img_y.to_ne_bytes()[2]); cursor += 1;
-    wav_buf.insert(cursor, img_y.to_ne_bytes()[3]);
+    let xb = img_x.to_ne_bytes();
+    let yb = img_y.to_ne_bytes();
+    let i2w_header = [b'i', b'2', b'w', b' ', 
+                      8, 0, 0, 0,
+                      xb[0], xb[1], xb[2], xb[3],
+                      yb[0], yb[1], yb[2], yb[3],];
+
+    wav_buf.splice(0x3c..0x3c, i2w_header.iter().cloned());
 
     let mut wav = File::create(path).expect("cant create file");
     wav.write_all(&wav_buf).expect("cant write");
@@ -437,31 +449,34 @@ struct Args {
     grayscale: bool,
     
     //wav2img args
-    stereo: bool,
+    mono: bool,
     
     //img2wav args
     dimensions: [i32; 2],
     rotate: usize,
     bit16: bool,
+    samplerate: u32,
 
     is_wav: bool,
     is_img: bool
 }
+
 fn main() {
 
     let mut args_struct = Args{
         input: "".to_string(), 
         output: "".to_string(), 
         grayscale: false,
-        stereo: false,
+        mono: false,
         dimensions: [0, 0],
         rotate: 0,
         bit16: false,
+        samplerate: 44100,
         is_wav: false, 
         is_img: false,
     };
 
-    process_args(&mut args_struct);
+    if !process_args(&mut args_struct) {return;};
 
     if args_struct.input.is_empty() {println!("No input file provided"); return};
     if args_struct.output.is_empty() {println!("No output file provided"); return};
